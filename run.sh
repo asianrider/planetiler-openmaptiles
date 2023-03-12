c16856605536aefe1a960477be12cac3d43f55c3
laurent <asianrider@gmail.com>
2023-03-12 19:38:32 +0100

    Add scripts to use the toolchain

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -Xmx20g -jar target/*with-deps.jar --force --download --area=planet --maxzoom=7 --languages=fr,de,it,en,ja,ru,es
---
+++ view.sh
@@ -0,0 +1,1 @@
+cd data; docker run --rm -it -v /Users/laurent/dev/org-planetiler/planetiler-openmaptiles:/data -p 8080:8080 maptiler/tileserver-gl
